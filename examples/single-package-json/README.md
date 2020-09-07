6178bc82810816972a195cc5efc2067f1755cc4e
Patrick Lehner <patrick.lehner@xitaso.com>
2020-09-07 09:28:56 +0200

    Add example of simple project with single package.json

---
+++ examples/single-package-json/README.md
@@ -0,0 +1,3 @@
+# DevCmd Example: Single `package.json`
+
+This is an example for simple projects that have only a single `package.json` file specifying both the project's npm dependencies, and `devcmd` (and other build script dependencies).
---
+++ examples/single-package-json/dev_cmds/example_cmd.ts
@@ -0,0 +1,24 @@
+import { devcmd } from "devcmd";
+
+(async () => {
+  console.log("Example command for multiple-package-jsons example");
+
+  await devcmd.exec({
+    command: "node",
+    args: ["-v"]
+  });
+
+  await devcmd.execParallel({
+    nodeVersion: {
+      command: "node",
+      args: ["-v"]
+    },
+    gitVersion: {
+      command: "git",
+      args: ["--version"]
+    }
+  });
+})().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
