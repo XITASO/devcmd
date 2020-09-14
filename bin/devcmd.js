6b34806aae851621286a66fd2a5522c1c7a1217d
Patrick Lehner <patrick.lehner@xitaso.com>
2020-09-14 11:59:24 +0200

    Add bin file that will use devcmd-cli

---
+++ bin/devcmd.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const { devcmdCli } = require("devcmd-cli");
+
+devcmdCli().catch(err => {
+  console.error("Uncaught promise rejection:", err);
+  process.exit(1);
+});
