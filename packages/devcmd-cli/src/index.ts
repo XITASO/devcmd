fbe5759b82b51631de812e3f520703ced334ab51
Patrick Lehner <patrick.lehner@xitaso.com>
2020-12-28 21:11:27 +0100

    Remove npx usage from devcmd-cli -- we can just run devcmd with plain node, it will then find any node_modules binaries on its own (see previous commit)

--- packages/devcmd-cli/src/index.ts
+++ packages/devcmd-cli/src/index.ts
@@ -48,24 +48,14 @@ async function isDir(path: string): Promise<boolean> {
   }
 }
 
-function isWindows(): boolean {
-  return process.platform === "win32";
-}
-
-function withCmdOnWin(baseCmd: string): string {
-  return isWindows() ? `${baseCmd}.cmd` : baseCmd;
-}
-
 // Note: if launching a node subprocess for the resolution should turn out to be a problem,
 //   we could also use the npm module "resolve" to find the path ourselves (and e.g. require it in-process).
 //   See https://yarnpkg.com/package/resolve
 async function runInDevCmdsDir(dirPath: string) {
   const [, , ...args] = process.argv;
-  const argString = args.map((s) => `"${s}"`).join(",");
 
   // TODO: use spawn or so instead
-  // we're using `npx` to launch node so that it automatically augments the PATH with the relevant `node_modules/.bin` dir(s)
-  execFileSync(withCmdOnWin("npx"), ["node", "-e", `require('devcmd').devcmd(${argString})`], {
+  execFileSync("node", ["-e", `require('devcmd').devcmd(...process.argv.slice(1))`, ...args], {
     cwd: dirPath,
     stdio: "inherit",
   });
