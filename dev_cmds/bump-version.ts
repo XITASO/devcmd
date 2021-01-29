2496427523305528be12fdfe6cf760d23981955a
Patrick Lehner <patrick.lehner@xitaso.com>
2021-01-29 17:19:45 +0100

    Revert changes to dev_cmds to allow commands to run with released devcmd version

--- dev_cmds/bump-version.ts
+++ dev_cmds/bump-version.ts
@@ -1,4 +1,4 @@
-import { execPiped, execToString, runAsyncMain } from "devcmd";
+import { execPiped, execToString } from "devcmd";
 import { blue, cyan, dim, green, yellow } from "kleur/colors";
 import fs from "fs-extra";
 import path from "path";
@@ -11,8 +11,9 @@ import {
   repoRoot,
   singlePackageJsonExampleDir,
 } from "./utils/paths";
+import { runAsyncMain } from "./utils/run_utils";
 
-async function main() {
+async function main(args: string[]) {
   const packageInfo = await selectPackage();
   console.log();
 
--- dev_cmds/test-all.ts
+++ dev_cmds/test-all.ts
@@ -1,6 +1,7 @@
-import { execPiped, runAsyncMain } from "devcmd";
+import { execPiped } from "devcmd";
 import { YARN_COMMAND } from "./utils/commands";
 import { repoRoot } from "./utils/paths";
+import { runAsyncMain } from "./utils/run_utils";
 
 async function main() {
   await execPiped({
