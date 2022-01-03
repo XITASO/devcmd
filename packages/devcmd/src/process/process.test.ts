a1aa831b1f8465cd34567929c56d7346ecdc664a
Patrick Lehner <lehner.pa@gmail.com>
2022-01-03 20:19:41 +0100

    Make handling of captured messages more readable and consistent in unit tests

--- packages/devcmd/src/process/process.test.ts
+++ packages/devcmd/src/process/process.test.ts
@@ -1,9 +1,6 @@
 import path from "path";
-import { isString } from "../utils/type_utils";
 import { ProcessExecutor } from ".";
 import { ConsoleLike } from "./ProcessExecutor";
-
-const ansiFormat = `(?:\\x1b\\[\\d+m)`;
 
 describe("ProcessExecutor", () => {
   describe("execInTty()", () => {
@@ -12,10 +9,11 @@ describe("ProcessExecutor", () => {
         const capturingConsole = new CapturingConsole();
         const execInTty = new ProcessExecutor(capturingConsole).execInTty;
         await execInTty({ command: "node", args: ["--version"] });
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(2);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Starting process/);
-        expect(stripAnsiColorSequences(consoleErrLines[1])).toMatch(/^Process "node" exited successfully/);
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(/^Starting process/);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited successfully/
+        );
       });
 
       test("process that successfully exits works (with no console)", async () => {
@@ -36,9 +34,9 @@ describe("ProcessExecutor", () => {
         const execInTty = new ProcessExecutor(capturingConsole).execInTty;
         const execPromise = execInTty({ command: "node", args: ["-e", "process.exit(2)"] });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -63,9 +61,9 @@ describe("ProcessExecutor", () => {
           options: { nonZeroExitCodeHandling: "printNoticeAndReturn" },
         });
         expect(result.exitCode).toBe(2);
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -79,7 +77,7 @@ describe("ProcessExecutor", () => {
           args: ["--version"],
           options: { suppressNotices: true },
         });
-        expect(capturingConsole.errorLines).toHaveLength(0);
+        expect(capturingConsole.errorMessages).toHaveLength(0);
       });
 
       test("failing executable prints and throws", async () => {
@@ -92,9 +90,10 @@ describe("ProcessExecutor", () => {
           options: { suppressNotices: true },
         });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(1);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Process "node" exited with status code 2/);
+        expect(capturingConsole.errorMessages).toHaveLength(1);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(
+          /^Process "node" exited with status code 2/
+        );
       });
     });
   });
@@ -105,10 +104,11 @@ describe("ProcessExecutor", () => {
         const capturingConsole = new CapturingConsole();
         const execPiped = new ProcessExecutor(capturingConsole).execPiped;
         await execPiped({ command: "node", args: ["--version"] });
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(2);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Starting process/);
-        expect(stripAnsiColorSequences(consoleErrLines[1])).toMatch(/^Process "node" exited successfully/);
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(/^Starting process/);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited successfully/
+        );
       });
 
       test("process that successfully exits works (with no console)", async () => {
@@ -129,9 +129,9 @@ describe("ProcessExecutor", () => {
         const execPiped = new ProcessExecutor(capturingConsole).execPiped;
         const execPromise = execPiped({ command: "node", args: ["-e", "process.exit(2)"] });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -156,11 +156,11 @@ describe("ProcessExecutor", () => {
           options: { nonZeroExitCodeHandling: "printNoticeAndReturn" },
         });
         expect(result.exitCode).toBe(2);
-        expect(capturingConsole.logLines).toHaveLength(1);
-        expect(capturingConsole.logLines[0].message).toBe("still works");
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.logMessages).toHaveLength(1);
+        expect(capturingConsole.logMessages[0]).toBe("still works");
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -174,7 +174,7 @@ describe("ProcessExecutor", () => {
           args: ["--version"],
           options: { suppressNotices: true },
         });
-        expect(capturingConsole.errorLines).toHaveLength(0);
+        expect(capturingConsole.errorMessages).toHaveLength(0);
       });
 
       test("failing executable prints and throws", async () => {
@@ -187,9 +187,10 @@ describe("ProcessExecutor", () => {
           options: { suppressNotices: true },
         });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(1);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Process "node" exited with status code 2/);
+        expect(capturingConsole.errorMessages).toHaveLength(1);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(
+          /^Process "node" exited with status code 2/
+        );
       });
     });
 
@@ -203,11 +204,9 @@ describe("ProcessExecutor", () => {
         args: ["-e", `console.log('${outputPrefix}' + process.cwd());`],
         options: { cwd: childProcessCwd },
       });
-      const outputLines = capturingConsole.logLines.filter(
-        (l) => isString(l.message) && l.message.startsWith(outputPrefix)
-      );
+      const outputLines = capturingConsole.logMessages.filter((l) => l.startsWith(outputPrefix));
       expect(outputLines).toHaveLength(1);
-      expect(outputLines[0].message).toEqual(`${outputPrefix}${childProcessCwd}`);
+      expect(outputLines[0]).toEqual(`${outputPrefix}${childProcessCwd}`);
     });
   });
 
@@ -220,9 +219,7 @@ describe("ProcessExecutor", () => {
           node: { command: "node", args: ["--version"] },
           node2: { command: "node", args: ["-e", "console.log('The other node process');"] },
         });
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines).map(
-          stripAnsiColorSequences
-        );
+        const consoleErrLines = capturingConsole.errorMessages.map(stripAnsiColorSequences);
         expect(consoleErrLines).toHaveLength(6);
         expect(consoleErrLines.filter((s) => s.includes("Starting process"))).toHaveLength(2);
         expect(consoleErrLines.filter((s) => s.includes(`exited successfully`))).toHaveLength(2);
@@ -261,13 +258,10 @@ describe("ProcessExecutor", () => {
           node2: { command: "node", args: ["-e", "console.log('The other node process');"] },
         });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines).map(
-          stripAnsiColorSequences
-        );
-        expect(consoleErrLines).toHaveLength(6);
-        const errorLine = consoleErrLines.filter((l) =>
-          l.startsWith(`<node> Process "node" exited with status code 2`)
-        );
+        expect(capturingConsole.errorMessages).toHaveLength(6);
+        const errorLine = capturingConsole.errorMessages
+          .map(stripAnsiColorSequences)
+          .filter((l) => l.startsWith(`<node> Process "node" exited with status code 2`));
         expect(errorLine).toHaveLength(1);
       });
 
@@ -296,16 +290,10 @@ describe("ProcessExecutor", () => {
           node2: { command: "node", args: ["-e", "console.log('The other node process');"] },
         });
         expect(results.node.exitCode).toBe(2);
-        expect(capturingConsole.errorLines).toHaveLength(6);
-        const errorLine = capturingConsole.errorLines.filter(
-          (l) => isString(l.message) && l.message.includes("exited with status code")
-        );
+        expect(capturingConsole.errorMessages).toHaveLength(6);
+        const errorLine = capturingConsole.errorMessages.filter((l) => l.includes("exited with status code"));
         expect(errorLine).toHaveLength(1);
-        expect(errorLine[0].message).toMatch(
-          new RegExp(
-            `^${ansiFormat}?<node> ${ansiFormat}?${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`
-          )
-        );
+        expect(stripAnsiColorSequences(errorLine[0])).toMatch(/^<node> Process "node" exited with status code 2/);
       });
 
       test("multiple failing processes prints and returns without throwing", async () => {
@@ -325,10 +313,8 @@ describe("ProcessExecutor", () => {
         ]);
         expect(results[0].exitCode).toBe(2);
         expect(results[1].exitCode).toBe(5);
-        expect(capturingConsole.errorLines).toHaveLength(6);
-        const errorLine = capturingConsole.errorLines.filter(
-          (l) => isString(l.message) && l.message.includes("exited with status code")
-        );
+        expect(capturingConsole.errorMessages).toHaveLength(6);
+        const errorLine = capturingConsole.errorMessages.filter((l) => l.includes("exited with status code"));
         expect(errorLine).toHaveLength(2);
       });
     });
@@ -344,10 +330,7 @@ describe("ProcessExecutor", () => {
           },
           { suppressNotices: true }
         );
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines).map(
-          stripAnsiColorSequences
-        );
-        expect(consoleErrLines).toHaveLength(0);
+        expect(capturingConsole.errorMessages).toHaveLength(0);
       });
     });
     describe("with option suppressNotices=true on single process", () => {
@@ -358,11 +341,10 @@ describe("ProcessExecutor", () => {
           node1: { command: "node", args: ["--version"], options: { suppressNotices: true } },
           node2: { command: "node", args: ["-e", "console.log('The other node process');"] },
         });
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines).map(
-          stripAnsiColorSequences
-        );
-        expect(consoleErrLines).toHaveLength(4);
-        expect(consoleErrLines.filter((s) => s.includes("node1"))).toHaveLength(0);
+        expect(capturingConsole.errorMessages).toHaveLength(4);
+        expect(
+          capturingConsole.errorMessages.map(stripAnsiColorSequences).filter((s) => s.includes("node1"))
+        ).toHaveLength(0);
       });
     });
   });
@@ -375,10 +357,11 @@ describe("ProcessExecutor", () => {
         const { stdout, stderr } = await execToString({ command: "node", args: ["--version"] });
         expect(stderr).toBe("");
         expect(stdout).toMatch(/v\d+\.\d+\.\d+/);
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(2);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Starting process/);
-        expect(stripAnsiColorSequences(consoleErrLines[1])).toMatch(/^Process "node" exited successfully/);
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(/^Starting process/);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited successfully/
+        );
       });
 
       test("process that successfully exits works (with no console)", async () => {
@@ -401,9 +384,9 @@ describe("ProcessExecutor", () => {
         const execToString = new ProcessExecutor(capturingConsole).execToString;
         const execPromise = execToString({ command: "node", args: ["-e", "process.exit(2)"] });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -429,9 +412,9 @@ describe("ProcessExecutor", () => {
         });
         expect(result.exitCode).toBe(2);
         expect(result.stdout).toBe("still works\n");
-        expect(capturingConsole.errorLines).toHaveLength(2);
-        expect(capturingConsole.errorLines[1].message).toMatch(
-          new RegExp(`^${ansiFormat}?Process "${ansiFormat}?node${ansiFormat}?" exited with status code 2`)
+        expect(capturingConsole.errorMessages).toHaveLength(2);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[1])).toMatch(
+          /^Process "node" exited with status code 2/
         );
       });
     });
@@ -447,7 +430,7 @@ describe("ProcessExecutor", () => {
         });
         expect(stderr).toBe("");
         expect(stdout).toMatch(/v\d+\.\d+\.\d+/);
-        expect(capturingConsole.errorLines).toHaveLength(0);
+        expect(capturingConsole.errorMessages).toHaveLength(0);
       });
 
       test("failing executable prints and throws", async () => {
@@ -460,9 +443,10 @@ describe("ProcessExecutor", () => {
           options: { suppressNotices: true },
         });
         await expect(execPromise).rejects.toThrowError("exited with status code 2");
-        const consoleErrLines = CapturingConsole.extractMessageStrings(capturingConsole.errorLines);
-        expect(consoleErrLines).toHaveLength(1);
-        expect(stripAnsiColorSequences(consoleErrLines[0])).toMatch(/^Process "node" exited with status code 2/);
+        expect(capturingConsole.errorMessages).toHaveLength(1);
+        expect(stripAnsiColorSequences(capturingConsole.errorMessages[0])).toMatch(
+          /^Process "node" exited with status code 2/
+        );
       });
     });
 
@@ -493,26 +477,35 @@ interface LogLine {
 }
 
 class CapturingConsole implements ConsoleLike {
-  private readonly _logLines: LogLine[] = [];
-  private readonly _errorLines: LogLine[] = [];
-  get logLines(): readonly LogLine[] {
-    return this._logLines;
+  private readonly _rawLogLines: LogLine[] = [];
+  private readonly _logMessages: string[] = [];
+  private readonly _rawErrorLines: LogLine[] = [];
+  private readonly _errorMessages: string[] = [];
+
+  get rawLogLines(): readonly LogLine[] {
+    return this._rawLogLines;
   }
-  get errorLines(): readonly LogLine[] {
-    return this._errorLines;
-  }
-  log(message?: any, ...optionalParams: any[]): void {
-    this._logLines.push({ message, optionalParams });
-  }
-  error(message?: any, ...optionalParams: any[]): void {
-    this._errorLines.push({ message, optionalParams });
+  get rawErrorLines(): readonly LogLine[] {
+    return this._rawErrorLines;
   }
 
-  static extractMessageStrings(logLines: readonly LogLine[]): string[] {
-    return logLines.map(({ message }) => message?.toString() ?? "");
+  get logMessages(): readonly string[] {
+    return this._logMessages;
+  }
+  get errorMessages(): readonly string[] {
+    return this._errorMessages;
+  }
+
+  log(message?: any, ...optionalParams: any[]): void {
+    this._rawLogLines.push({ message, optionalParams });
+    this._logMessages.push((message ?? "").toString());
+  }
+  error(message?: any, ...optionalParams: any[]): void {
+    this._rawErrorLines.push({ message, optionalParams });
+    this._errorMessages.push((message ?? "").toString());
   }
 }
 
 function stripAnsiColorSequences(s: string): string {
-  return s.replace(new RegExp(ansiFormat, "g"), "");
+  return s.replace(/(?:\x1b\[\d+m)/g, "");
 }
